78811e141878f38c99dc578ef6b63e43e805692b
Guyeon Chung <gouyeonch@naver.com>
2024-07-20 20:29:58 +0900

    FEAT : 공통 DTO, exception handler

--- src/main/java/org/dongguk/camputhon/controller/TestController.java
+++ src/main/java/org/dongguk/camputhon/controller/TestController.java
@@ -2,6 +2,9 @@ package org.dongguk.camputhon.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dongguk.camputhon.dto.common.ResponseDto;
+import org.dongguk.camputhon.exception.CommonException;
+import org.dongguk.camputhon.exception.ErrorCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/test")
+@RequestMapping("")
 public class TestController {
-    @GetMapping("")
-    public String test() {
-        return "Hello World!";
+    @GetMapping("/test")
+    public ResponseDto<?> test() {
+        return ResponseDto.ok("Hello World");
+    }
+
+    @GetMapping("/error")
+    public ResponseDto<?> error() {
+        throw new CommonException(ErrorCode.SERVER_ERROR);
     }
 }
